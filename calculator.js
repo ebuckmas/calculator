5e499e7ed703bdac7d0a487ada3b1ff1a83666a8
ebuckmas <evanbuckmaster@gmail.com>
2022-07-05 11:29:01 -0400

    created basic math functions and operator function

--- calculator.js
+++ calculator.js
@@ -1,1 +1,13 @@
-console.log('cool');
+//create basic math functions
+
+const add = (x,y) => x + y;
+
+const subtract = (x,y) => x - y;
+
+const multiply = (x,y) => x * y;
+
+const divide = (x,y) => x / y;
+
+//create operator function that takes in operation to perform plus two numbers
+
+const operate = (operator, x, y) => operator(x,y);
